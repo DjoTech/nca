512ea9c141c37d201d38f9fa1e8c22fd6dca6730
Andreas Jonathan <anddreasjo10@gmail.com>
2024-05-01 21:15:17 +0700

    initial commit

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,16 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
 
-  isPositionAbsolute = false
+  isHomepage = false
+
+  constructor(
+    private router: Router
+  ) {
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.isHomepage = location.pathname === '/home'
+      }
+    })
+  }
 
   ngOnInit(): void {
-    this.isPositionAbsolute = location.pathname === 'home'
   }
 
 }
