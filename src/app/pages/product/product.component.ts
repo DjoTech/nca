c858edb8d198f66315b37f12d1fd9aaffd74899b
Renaldy002 <oshaleon581@Gmail.com>
2024-11-16 01:30:11 +0700

    Update Product data

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import {MOBILE_BREAKPOINT, PRODUCT, RESPONSIVE_OPTIONS, TABLE_BREAKPOINT} from "../../../data/constant";
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-
+  product = PRODUCT
 
   selectedProduct = 3;
 
