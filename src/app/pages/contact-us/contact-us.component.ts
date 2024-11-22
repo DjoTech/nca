d324dfc3c18c1fc37dd56585737a370e341016e0
Andreas Jonathan <anddreasjo10@gmail.com>
2024-11-22 22:35:49 +0700

    information & about us

---
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactUsComponent {
+
+}
